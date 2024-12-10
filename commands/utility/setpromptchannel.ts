4116b935a02e2a69fa4530684ff144bda830d877
git <82054121+KXzeno@users.noreply.github.com>
2024-12-10 02:52:31 -0800

    fix: rectify termination predicate

--- commands/utility/setpromptchannel.ts
+++ commands/utility/setpromptchannel.ts
@@ -30,7 +30,7 @@ export const command = {
 		if (!interaction.guildId || !interaction.guild) throw new Error('Guild undetected.');
 		if (!channel) throw new Error('Elected channel undetected.');
 
-		if ((terminate && prompt.dailyIntvId !== null) || (terminate && targetChannel !== null)) {
+		if ((terminate && prompt.dailyIntvId !== null) || (terminate && targetChannel && (targetChannel as Config).promptChannelId.includes("XNULL") === false)) {
 			interaction.reply({ content: 'Prompter terminated.', ephemeral: true});
 
 			let nullifyCh = await prisma.config.update({
